de89cf6ff913d899f07f495e9b16c5b0212ed66c
martinbicycle <martinbicycle7700@gmail.com>
2020-12-31 06:06:20 +0000

    [Add]コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+  end
+  # resource :favorites, only: [:create, :destroy]
 
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
